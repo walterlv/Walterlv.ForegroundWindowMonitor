1d7b557eb2f0177f8feecb1ff63587ad0f04d37a
walterlv <xblero@gmail.com>
2023-08-02 11:01:01 +0800

    添加项目文件。

---
+++ Walterlv.ForegroundWindowMonitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using Walterlv.ForegroundWindowMonitor;
+
+using Windows.Win32.Foundation;
+
+using static Windows.Win32.PInvoke;
+
+HWND last = default;
+while (true)
+{
+    var current = GetForegroundWindow();
+    if (current != last)
+    {
+        last = current;
+        var w = new Win32Window(current);
+        //var (hWnd, className, title) = GetWindowInfo(current);
+        //GetWindowThreadProcessId(hWnd, out var pid);
+        Console.WriteLine($"[{DateTime.Now:mm:ss}] [pid={w.ProcessId}] [{w.Handle:X8}] {w.Title} - {w.ClassName}");
+    }
+    await Task.Delay(200).ConfigureAwait(false);
+}
---
+++ Walterlv.ForegroundWindowMonitor/Win32Window.cs
@@ -0,0 +1,51 @@
+﻿using System.Buffers;
+
+using Windows.Win32.Foundation;
+
+using static Windows.Win32.PInvoke;
+
+namespace Walterlv.ForegroundWindowMonitor;
+public class Win32Window
+{
+    private readonly HWND _hWnd;
+    private string? _className;
+    private string? _title;
+    private uint _pid;
+
+    internal Win32Window(nint handle)
+    {
+        _hWnd = (HWND)handle;
+    }
+
+    public nint Handle => _hWnd;
+
+    public string ClassName => _className ??= CallWin32ToGetPWSTR(512, (p, l) => GetClassName(_hWnd, p, l));
+
+    public string Title => _title ??= CallWin32ToGetPWSTR(512, (p, l) => GetWindowText(_hWnd, p, l));
+
+    public uint ProcessId => _pid is 0 ? (_pid = GetProcessIdCore()) : _pid;
+
+    private unsafe uint GetProcessIdCore()
+    {
+        uint pid = 0;
+        GetWindowThreadProcessId(_hWnd, &pid);
+        return pid;
+    }
+
+    private unsafe string CallWin32ToGetPWSTR(int bufferLength, Func<PWSTR, int, int> getter)
+    {
+        var buffer = ArrayPool<char>.Shared.Rent(bufferLength);
+        try
+        {
+            fixed (char* ptr = buffer)
+            {
+                getter(ptr, bufferLength);
+                return new string(buffer);
+            }
+        }
+        finally
+        {
+            ArrayPool<char>.Shared.Return(buffer);
+        }
+    }
+}
