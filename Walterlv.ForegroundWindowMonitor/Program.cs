bde50c40e2a2054247fce13e208f3e9c3810a4fa
walterlv <xblero@gmail.com>
2023-08-02 18:18:45 +0800

    监听前台窗口的变化，而不是轮询

--- Walterlv.ForegroundWindowMonitor/Program.cs
+++ Walterlv.ForegroundWindowMonitor/Program.cs
@@ -1,55 +1,52 @@
-﻿using System.Text;
-
+﻿#pragma warning disable CA1416 // 验证平台兼容性
 using Walterlv.ForegroundWindowMonitor;
 
 using Windows.Win32.Foundation;
+using Windows.Win32.UI.Accessibility;
 
 using static Windows.Win32.PInvoke;
 
-Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
+// 输出表头。
 var consoleWidth = Console.WindowWidth;
 var table = new ConsoleTableBuilder<Win32Window>(consoleWidth, new ConsoleTableColumnDefinition<Win32Window>[]
 {
     (8, "time", _ => $"{DateTime.Now:hh:mm:ss}"),
     (8, "hwnd", w => $"{w.Handle:X8}"),
-    (6, "pid", w => $"{w.ProcessId}"),
     (0.5, "title", w => w.Title),
     (0.25, "class name", w => w.ClassName),
+    (6, "pid", w => $"{w.ProcessId}"),
     (0.25, "process name", w => $"{w.ProcessName}"),
 });
 Console.WriteLine(table.BuildHeaderRows());
 
-HWND last = default;
-for (var i = 1; i < int.MaxValue; i++)
+// 监听系统的前台窗口变化。
+SetWinEventHook(
+    EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND,
+    HMODULE.Null, WinEventProc,
+    0, 0,
+    WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS);
+
+// 开启消息循环，以便 WinEventProc 能够被调用。
+if (GetMessage(out var lpMsg, default, default, default))
+{
+    TranslateMessage(in lpMsg);
+    DispatchMessage(in lpMsg);
+}
+
+// 当前前台窗口变化时，输出新的前台窗口信息。
+void WinEventProc(HWINEVENTHOOK hWinEventHook, uint @event, HWND hwnd, int idObject, int idChild, uint idEventThread, uint dwmsEventTime)
 {
     var current = GetForegroundWindow();
-    if (current != last)
+    var newConsoleWidth = Console.WindowWidth;
+    if (newConsoleWidth != consoleWidth)
     {
-        var newConsoleWidth = Console.WindowWidth;
-        if (newConsoleWidth != consoleWidth)
-        {
-            consoleWidth = newConsoleWidth;
-            table = table.NewTableWidth(newConsoleWidth);
-            Console.WriteLine(table.BuildHeaderRows());
-        }
-
-        last = current;
-        var w = new Win32Window(current);
-        var rowText = table.BuildRow(w, StringDisplayMode.Wrap);
-
-        Console.WriteLine(rowText);
-
-        //if (i % 2 is 0)
-        //{
-        //    Console.BackgroundColor = ConsoleColor.DarkGray;
-        //    Console.ForegroundColor = ConsoleColor.Black;
-        //}
-        //else
-        //{
-        //    Console.BackgroundColor = ConsoleColor.Black;
-        //    Console.ForegroundColor = ConsoleColor.White;
-        //}
+        consoleWidth = newConsoleWidth;
+        table = table.NewTableWidth(newConsoleWidth);
+        Console.WriteLine(table.BuildHeaderRows());
     }
-    await Task.Delay(200).ConfigureAwait(false);
+
+    var w = new Win32Window(current);
+    var rowText = table.BuildRow(w, StringDisplayMode.Wrap);
+
+    Console.WriteLine(rowText);
 }
